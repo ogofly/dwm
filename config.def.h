f2f0e2e7cc4d05db35a2c67dcb27ffa6258b8256
ogofly <ogofly@outlook.com>
2026-01-11 16:06:49 +0800

    my conf

--- config.def.h
+++ config.def.h
@@ -83,11 +83,12 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "fzfmenu", NULL,     "fzf", 	0,     1,         1,           1,        -1 }, /* xev */
+	{ "fzfmenu", NULL,     "fzf",          0,         1,          1,           1,        -1 }, /* xev */
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ NULL,      NULL,     "Weixin", 0,    1,         0,           1,        -1 }, /* xev */
-	{ NULL,      NULL,     "微信", 0,      1,         0,           1,        -1 }, /* xev */
-	{ "float",      NULL,     NULL,           0,         1,          1,           0,        -1 },
+	{ "wechat",  NULL,     NULL, 0,        1,         0,          1,           -1 }, /* xev */
+	{ NULL,      NULL,     "微信", 0,      1,         0,          1,           -1 }, /* xev */
+	{ "float",   NULL,     NULL,           0,         1,          1,           0,        -1 },
+	{ NULL,      NULL,     "Fcitx",        0,         1,          0,           1,        -1 }
 };
 
 #include "vanitygaps.c"
